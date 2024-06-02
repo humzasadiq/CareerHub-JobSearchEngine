4eac89de09bfd099be92a8419530e1047c4ad6c2
humzasadiq <humzasadiq@gmail.com>
2024-06-02 20:58:35 +0500

    uibuild

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const linkedIn = require('linkedin-jobs-api'); // Make sure this is correctly installed and configured
 const cors = require('cors');
+const axios = require('axios');
 
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 app.use(cors());
 app.use(express.json());
 
@@ -22,7 +23,6 @@ app.post('/api/jobs', async (req, res) => {
         limit: limit, // Convert limit to a number
         sortBy
     };
-    console.log(queryOptions);
     try {
         const response = await linkedIn.query(queryOptions); // Query LinkedIn API
         res.json(response); // Send job listings data to the frontend
@@ -31,6 +31,33 @@ app.post('/api/jobs', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch job listings' });
     }
 });
+
+app.post('/api/jobs1', async (req, res) => {
+    const { query, date_posted, page} = req.body;
+    const options = {
+        method: 'GET',
+        url: 'https://jsearch.p.rapidapi.com/search',
+        headers: {
+        'X-RapidAPI-Key': 'b03a04e701msh8d1955b60a71041p12f45ajsnf531f5a0bc5e',
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+        },
+        params: {
+        query: query,
+        page: page,
+        num_pages: '1',
+        date_posted: date_posted
+        
+        },
+    };
+    console.log(options);
+    try {
+        const response = await axios.request(options);
+        const data = response.data.data;
+        res.json(data);
+        } catch (error) {
+        console.error('ERROR', error);
+      }
+})
 
 app.use(unknownEndpoint);
 
