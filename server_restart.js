c0043fecaf8ed9706fbb077a0985081b0d60eb4e
humzasadiq <humzasadiq@gmail.com>
2024-06-05 22:57:09 +0500

    uibuild

--- server_restart.js
+++ server_restart.js
@@ -1,25 +1,22 @@
-const { error } = require('console');
-const cron = require('cron')
-const https = require('https')
+const cron = require('cron');
+const https = require('https');
 
 const backendUrl = 'https://careerhub-l7oq.onrender.com';
-const job = new cron.CronJob('*/14****', function() {
+
+const job = new cron.CronJob('*/14 * * * *', function() {
     console.log('Restarting the server');
 
-    https
-        .get(backendUrl, (res) =>{
-            if(res.statusCode === 200){
-                console.log('Server Restarted');
-            } else {
-                console.error(`failed to res server; Status code: ${res.statusCode}`
-                );
-            }
-        })
-        .on('error', (err) => {
-            console.error('Error:', err.message);
-        });
+    https.get(backendUrl, (res) => {
+        if (res.statusCode === 200) {
+            console.log('Server Restarted');
+        } else {
+            console.error(`Failed to restart server; Status code: ${res.statusCode}`);
+        }
+    }).on('error', (err) => {
+        console.error('Error:', err.message);
+    });
 });
 
 module.exports = {
-    job,
-}
+    start: () => job.start()
+};
